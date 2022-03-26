bc69ce9056a881a890d327b826baea692502faa9
beobiebom <beobiebom@gmail.com>
2022-03-26 10:25:54 +0700

    refactor structure folder and add model for call api

---
+++ app/src/main/java/com/beo/foody/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.beo.foody.util
+
+class Constants {
+    companion object {
+        const val BASE_URL = "https://api.spoonacular.com"
+        const val API_KEY = "27591e9be9d7441eba4bef675e4bfb25"
+    }
+}
